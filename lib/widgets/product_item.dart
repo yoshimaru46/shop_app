83aeb385ec22ea5a59e4350995558758361c521d
Yoshihiro Saito <yshr446@gmail.com>
2020-01-04 22:42:59 +0900

    Add product overview screen

---
+++ lib/widgets/product_item.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app/screens/products_detail_screen.dart';
+
+class ProductItem extends StatelessWidget {
+  final String id;
+  final String title;
+  final String imageUrl;
+
+  ProductItem(this.id, this.title, this.imageUrl);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        child: GestureDetector(
+          onTap: () {
+            Navigator.of(context).push(MaterialPageRoute(
+                builder: (ctx) => ProductsDetailViewScreen()));
+          },
+          child: Image.network(
+            imageUrl,
+            fit: BoxFit.cover,
+          ),
+        ),
+        footer: GridTileBar(
+          backgroundColor: Colors.black87,
+          leading: IconButton(
+            icon: Icon(
+              Icons.favorite,
+              color: Theme.of(context).accentColor,
+            ),
+          ),
+          title: Text(
+            title,
+            textAlign: TextAlign.center,
+          ),
+          trailing: IconButton(
+            icon:
+                Icon(Icons.shopping_cart, color: Theme.of(context).accentColor),
+          ),
+        ),
+      ),
+    );
+  }
+}
